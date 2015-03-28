43985ef2d424d58d1c0da4ac452a3075af5edc7b
a254102008 <a254102008@yahoo.com.tw>
2015-03-28 13:22:22 +0800

    test1

--- src/test.java
+++ src/test.java
@@ -7,7 +7,7 @@ public class test
 	{ 
 		try 
 		{ 
-			Process p=Runtime.getRuntime().exec("cmd /c ../dos.bat"); 
+			Process p=Runtime.getRuntime().exec("../dos.bat"); 
 			p.waitFor(); 
 			BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
 			String line=reader.readLine(); 
