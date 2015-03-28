d4dc9eccbe9a5d73ca35fcdc919b6216f37706d5
a254102008 <a254102008@yahoo.com.tw>
2015-03-28 11:26:47 +0800

    init

---
+++ src/test.java
@@ -0,0 +1,24 @@
+
+
+import java.io.*; 
+
+public class test 
+{ 
+	public static void main(String args[]) 
+	{ 
+		try 
+		{ 
+			Process p=Runtime.getRuntime().exec("cmd /c C:/Users/user1/workspace/test/dos.bat"); 
+			p.waitFor(); 
+			BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+			String line=reader.readLine(); 
+			while(line!=null) 
+			{ 
+				System.out.println(line); 
+				line=reader.readLine(); 
+			} 
+		} 
+		catch(IOException e1) {} 
+		catch(InterruptedException e2) {} 
+	} 
+} 
